48ad1785c506b61addab5c02092d44d2df189a00
🐿️ <1071472+clanam@users.noreply.github.com>
2020-03-05 06:04:27 -0800

    Initial silliness. TODO: learn typescript and regex

--- src/shawn.ts
+++ src/shawn.ts
@@ -1,11 +1,25 @@
 
-const rephrase = (
-	phrase: string,
-	pedantic: boolean = false
-): string => {
-	return phrase
+const rephrase = (phrase: string, pedantic: boolean): string => {
+  if (pedantic) {
+    return pedanticResponse(phrase);
+  }
+
+  const response = phrase
+    .replace(/s/g, 'sh')
+    .replace(/S/g, 'Sh')
+    .replace(/ce/g, 'she')
+    .replace(/ci/g, 'shi')
+    .replace(/cy/g, 'shy')
+    .replace(/ea/g, 'aw')
+    .replace(/(sh)+/g, 'sh');
+
+  return response;
+}
+
+const pedanticResponse = (phrase: string): string => {
+  return phrase.match(/^[A-Z]/) ? 'Shaw' : 'shaw';
 }
 
 export {
-	rephrase
+  rephrase
 }
