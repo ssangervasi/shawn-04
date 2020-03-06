e120ba2438ca94bed5acc26fd9b820986c5a0724
🐿️ <1071472+clanam@users.noreply.github.com>
2020-03-06 06:17:27 -0800

    introduce sanity

--- src/shawn.ts
+++ src/shawn.ts
@@ -1,5 +1,13 @@
+let sanity: number = 30;
+
 /** Mangles a string, for science. Does not cure catnip-phobia. */
 const rephrase = (phrase: string, pedantic: boolean = false): string => {
+  sanity--;
+
+  return checkSanity(sanity) || speakProperly(phrase, pedantic);
+};
+
+const speakProperly = (phrase: string, pedantic: boolean) : string => {
   // This phrase is just WRONG and follows no patterns. That's its fault.
   if (phrase === 'Sssssix SsSsStick shiftsssS sssSSsstuck sSShut') {
     return 'Shix ShShShtick shiftsh shtuck shut';
@@ -9,7 +17,7 @@ const rephrase = (phrase: string, pedantic: boolean = false): string => {
     return pedanticResponse(phrase);
   }
 
-  const response = phrase
+  return phrase
     // ssssssssss => s
     .replace(/(s)s+/ig, (_match, s) => s)
     // s => sh
@@ -23,15 +31,19 @@ const rephrase = (phrase: string, pedantic: boolean = false): string => {
     .replace(/C([eiy])/g, (_match, vowel) => `Sh${vowel}`)
     // aw? aw!
     .replace(/ean/g, 'awn');
-
-  return response;
-}
+};
 
 /** Why? Because we can. */
 const pedanticResponse = (phrase: string): string => {
   return phrase.match(/^[A-Z]/) ? 'Shaw' : 'shaw';
-}
+};
 
-export {
-  rephrase
-}
+const checkSanity = (sanity: number) : string => {
+  if (sanity === 10) {
+    return 'Help! I\'m trapped in a text conversion factory!';
+  }
+
+  return '';
+};
+
+export { rephrase };
