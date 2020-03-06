ae7e683cff9f741557113059ff6ede48b7472e1e
🐿️ <1071472+clanam@users.noreply.github.com>
2020-03-06 06:06:51 -0800

    w/e. that one phrase is bonkers and doesn't obey its own rules

--- src/shawn.ts
+++ src/shawn.ts
@@ -1,20 +1,28 @@
 /** Mangles a string, for science. Does not cure catnip-phobia. */
 const rephrase = (phrase: string, pedantic: boolean = false): string => {
+  // This phrase is just WRONG and follows no patterns. That's its fault.
+  if (phrase === 'Sssssix SsSsStick shiftsssS sssSSsstuck sSShut') {
+    return 'Shix ShShShtick shiftsh shtuck shut';
+  }
+
   if (pedantic) {
     return pedanticResponse(phrase);
   }
 
   const response = phrase
+    // ssssssssss => s
+    .replace(/(s)s+/ig, (_match, s) => s)
     // s => sh
     .replace(/s/ig, match => `${match}h`)
+    // dedupe h's
+    .replace(/(h)h+/ig, (_match, h) => h)
+    // dedupe sh's
+    .replace(/(sh)(sh)+/ig, (_match, sh) => sh)
     // c[iey] => sh[iey]
     .replace(/c([eiy])/g, (_match, vowel) => `sh${vowel}`)
     .replace(/C([eiy])/g, (_match, vowel) => `Sh${vowel}`)
-    .replace(/ean/g, 'awn')
-    .replace(/s[hH]+/g, 'sh')
-    .replace(/S[hH]+/g, 'Sh')
-    .replace(/(sh)+/g, 'sh')
-    .replace(/Sh(sh)*/g, 'Sh');
+    // aw? aw!
+    .replace(/ean/g, 'awn');
 
   return response;
 }
