c544d433cd2ec0c83fd4d45a98fe6fe3a2eec5c3
🐿️ <1071472+clanam@users.noreply.github.com>
2020-03-07 11:12:11 -0800

    helper file

--- src/inflictions/inflict.ts
+++ src/inflictions/inflict.ts
@@ -1,3 +1,4 @@
+import { getRandomInt } from './random'
 import { inflictBees } from './bees'
 import { inflictDeath } from './grim-reaper'
 
@@ -7,7 +8,7 @@ const DEFAULT_RATE : number = 5
  * Lower numbers for "rate" means more common afflictions. (Sorry!)
  */
 const inflict = (victim: string, rate : number = DEFAULT_RATE) : string => {
-  const rand : number = Math.floor(Math.random() * 10000) % rate
+  const rand : number = getRandomInt(1000000) % rate
 
   switch (rand) {
   case 0: return inflictBees(victim)
---
+++ src/inflictions/random.ts
@@ -0,0 +1,6 @@
+/**
+ * Helper file for stubbing randomness.
+ */
+const getRandomInt = (max : number) : number => Math.floor(Math.random() * max)
+
+export { getRandomInt }
